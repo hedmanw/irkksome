c13b860ec708316b28b95fd0836bf207a7c73967
Wilhelm Hedman <wilhelm.hedman@ctk.se>
2014-07-18 18:17:51 +0200

    Cleaned up DAO and made responsibility more clear

--- app/src/main/java/se/alkohest/irkksome/model/api/dao/IrcUserDAO.java
+++ app/src/main/java/se/alkohest/irkksome/model/api/dao/IrcUserDAO.java
@@ -1,19 +1,22 @@
 package se.alkohest.irkksome.model.api.dao;
 
+import android.content.ContentValues;
+
 import se.alkohest.irkksome.model.api.local.IrcUserDAOLocal;
 import se.alkohest.irkksome.model.entity.IrcUser;
 import se.alkohest.irkksome.model.impl.IrcUserEB;
 import se.alkohest.irkksome.orm.GenericDAO;
 
 public class IrcUserDAO extends GenericDAO<IrcUserEB, IrcUser> implements IrcUserDAOLocal {
-    @Override
-    protected IrcUser create() {
-        return new IrcUserEB();
-    }
 
     public IrcUser create(String name) {
         IrcUser ircUser = new IrcUserEB();
         ircUser.setName(name);
         return ircUser;
+    }
+
+    @Override
+    protected ContentValues createContentValues(IrcUser bean) {
+        return null;
     }
 }
--- app/src/main/java/se/alkohest/irkksome/orm/GenericDAO.java
+++ app/src/main/java/se/alkohest/irkksome/orm/GenericDAO.java
@@ -1,7 +1,22 @@
 package se.alkohest.irkksome.orm;
 
+import android.content.ContentValues;
+
 public abstract class GenericDAO<E extends AbstractBean, I extends BeanEntity> {
     protected static PersistenceContext persistenceContext;
+    private String table;
 
-    protected abstract I create();
+    public GenericDAO() {
+        // get table from Annotation on E
+    }
+
+    protected void makePersistent(I beanEntity) throws ORMException {
+        persistenceContext.create(table, createContentValues(beanEntity));
+    }
+
+    protected void makeTransient(I beanEntity) throws ORMException {
+        persistenceContext.delete();
+    }
+
+    protected abstract ContentValues createContentValues(I beanEntity);
 }
