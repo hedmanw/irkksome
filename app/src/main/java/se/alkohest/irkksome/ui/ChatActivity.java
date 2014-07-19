6c0c132364e83be02a5def53c551193d6b52d5ad
Wilhelm Hedman <hedmanw@student.chalmers.se>
2014-07-19 21:08:12 +0200

    Removed obsolete imports // Hej grabbar!

--- app/src/main/java/se/alkohest/irkksome/ui/ChatActivity.java
+++ app/src/main/java/se/alkohest/irkksome/ui/ChatActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import se.alkohest.irkksome.R;
 import se.alkohest.irkksome.model.api.Server;
