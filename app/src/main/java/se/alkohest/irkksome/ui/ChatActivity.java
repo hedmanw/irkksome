ffaf11123a72f3e7229acfeb893384f374852642
Joel Torstensson <oed3933@gmail.com>
2014-07-19 21:38:02 +0200

    Can now send messages.

--- app/src/main/java/se/alkohest/irkksome/ui/ChatActivity.java
+++ app/src/main/java/se/alkohest/irkksome/ui/ChatActivity.java
@@ -4,21 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import se.alkohest.irkksome.R;
 import se.alkohest.irkksome.model.api.Server;
 import se.alkohest.irkksome.model.api.ServerManager;
+import se.alkohest.irkksome.model.entity.IrcChannel;
 
 public class ChatActivity extends Activity {
     private ServerManager serverManager;
+    private Server server;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         serverManager = new ServerManager();
-        Server server = serverManager.addServer("irc.chalmers.it");
+        server = serverManager.addServer("irc.chalmers.it");
     }
 
     @Override
@@ -38,5 +42,11 @@ public class ChatActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        IrcChannel channel = server.joinChannel("#irkksome");
+        EditText editText = (EditText) findViewById(R.id.input_field);
+        server.sendMessage(channel, editText.getText().toString());
     }
 }
