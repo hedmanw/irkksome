7ffeff029760e6d51e71879809d4238aa8e4267d
Joel Torstensson <oed3933@gmail.com>
2014-07-30 01:13:44 +0200

    Irssi script now logs more data, it also clears after sending backlog.

--- irssi/irkksome.pl
+++ irssi/irkksome.pl
@@ -1,11 +1,11 @@
 use Irssi;
 
-$VERSION = "0.1";
+$VERSION = "0.2";
 %IRSSI = (
-    authors         => "oed",
     name            => "irkksome",
+    description     => "Integrates irssi with the irkksome android app.",
+    authors         => "oed",
     url             => "http://github.com/ircsex/irkksome",
-    description     => "This script integrates irssi with irkksome.",
     license         => "Beerware",
     changed         => "2014-07-29"
 );
@@ -24,9 +24,14 @@ sub send_backlog {
         Irssi::signal_emit('server incoming', $server, $line);
     }
     close(LOG);
+    clear_backlog();
 
     Irssi::signal_remove('print text', 'stop_signal');
     signals_add();
+}
+
+sub clear_backlog {
+    unlink Irssi::settings_get_str('irkksome_log');
 }
 
 sub stop_signal {
@@ -74,6 +79,26 @@ sub own_private {
     log_string(":$server->{nick}! PRIVMSG $target :$msg");
 }
 
+sub join {
+    my ($server, $channel, $nick, $address) = @_;
+    log_string(":$nick!$address JOIN :$channel")
+}
+
+sub part {
+    my ($server, $channel, $nick, $address, $reason) = @_;
+    log_string(":$nick!$address PART $channel")
+}
+
+sub quit {
+    my ($server, $nick, $address, $reason) = @_;
+    log_string(":$nick!$address QUIT :$channel")
+}
+
+sub nick {
+    my ($server, $new, $old, $address) = @_;
+    log_string("$old!$address NICK :$new")
+}
+
 # Signals
 sub signals_add {
     Irssi::signal_add('proxy client command', 'handle_command');
@@ -81,12 +106,12 @@ sub signals_add {
     Irssi::signal_add_last('message private', 'private');
     Irssi::signal_add_last('message own_public', 'own_public');
     Irssi::signal_add_last('message own_private', 'own_private');
-    #Irssi::signal_add_last('message join', 'join');
-    #Irssi::signal_add_last('message part', 'part');
-    #Irssi::signal_add_last('message quit', 'quit');
+    Irssi::signal_add_last('message join', 'join');
+    Irssi::signal_add_last('message part', 'part');
+    Irssi::signal_add_last('message quit', 'quit');
     #Irssi::signal_add_last('message kick', 'kick');
-    #Irssi::signal_add_last('message nick', 'nick');
-    #Irssi::signal_add_last('message own_nick', 'own_nick');
+    Irssi::signal_add_last('message nick', 'nick');
+    Irssi::signal_add_last('message own_nick', 'nick');
     #Irssi::signal_add_last('message invite', 'invite');
     #Irssi::signal_add_last('message topic', 'topic');
 }
@@ -97,12 +122,12 @@ sub signals_remove {
     Irssi::signal_remove('message private', 'private');
     Irssi::signal_remove('message own_public', 'own_public');
     Irssi::signal_remove('message own_private', 'own_private');
-    #Irssi::signal_remove('message join', 'join');
-    #Irssi::signal_remove('message part', 'part');
-    #Irssi::signal_remove('message quit', 'quit');
+    Irssi::signal_remove('message join', 'join');
+    Irssi::signal_remove('message part', 'part');
+    Irssi::signal_remove('message quit', 'quit');
     #Irssi::signal_remove('message kick', 'kick');
-    #Irssi::signal_remove('message nick', 'nick');
-    #Irssi::signal_remove('message own_nick', 'own_nick');
+    Irssi::signal_remove('message nick', 'nick');
+    Irssi::signal_remove('message own_nick', 'nick');
     #Irssi::signal_remove('message invite', 'invite');
     #Irssi::signal_remove('message topic', 'topic');
 }
