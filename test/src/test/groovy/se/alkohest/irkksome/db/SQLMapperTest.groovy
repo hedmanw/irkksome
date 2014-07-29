9ce164c54c48071bfd1da2824a526355dc86b264
Wilhelm Hedman <w.hedman@gmail.com>
2014-07-29 19:32:57 +0200

    Pimped out SQLMapperTest with Groovy Goodness

--- test/src/test/groovy/se/alkohest/irkksome/db/SQLMapperTest.groovy
+++ test/src/test/groovy/se/alkohest/irkksome/db/SQLMapperTest.groovy
@@ -19,12 +19,16 @@ public class SQLMapperTest extends Specification {
         def server = "CREATE TABLE t_server(id INTEGER PRIMARY KEY AUTOINCREMENT, host TEXT NOT NULL, self INTEGER NOT NULL);"
         def channel = "CREATE TABLE t_channel(id INTEGER PRIMARY KEY AUTOINCREMENT, topic TEXT NOT NULL, name TEXT NOT NULL, server_id INTEGER NOT NULL);"
         def message = "CREATE TABLE t_message(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT NOT NULL, author INTEGER NOT NULL, hilight BOOLEAN NOT NULL, channel_id INTEGER NOT NULL);"
+        int length = 0;
+        createStatement.each {
+            length += it.length()
+        }
 
         then:
         createStatement.contains(user)
         createStatement.contains(server)
         createStatement.contains(channel)
         createStatement.contains(message)
-//        createStatement.length() == user.length() + server.length() + channel.length() + message.length()
+        length == user.length() + server.length() + channel.length() + message.length()
     }
 }
