0699430edef93d9f3c770689ab2c2acecb6773bc
Wilhelm Hedman <w.hedman@gmail.com>
2014-08-06 21:32:56 +0200

    Hooks into presses of return key on IME

--- app/src/main/java/se/alkohest/irkksome/ui/ChannelFragment.java
+++ app/src/main/java/se/alkohest/irkksome/ui/ChannelFragment.java
@@ -3,11 +3,15 @@ package se.alkohest.irkksome.ui;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import se.alkohest.irkksome.R;
 import se.alkohest.irkksome.model.entity.IrcChannel;
@@ -44,6 +48,17 @@ public class ChannelFragment extends Fragment {
         final View inflatedView = inflater.inflate(R.layout.fragment_channel, container, false);
         messageList = (ListView) inflatedView.findViewById(R.id.listView);
         messageList.setAdapter(arrayAdapter);
+        EditText messageEditText = ((EditText) inflatedView.findViewById(R.id.input_field));
+        messageEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int keyCode, KeyEvent keyEvent) {
+                if (keyCode == EditorInfo.IME_ACTION_DONE) {
+                    // send message
+                    return true;
+                }
+                return false;
+            }
+        });
         scrollToBottom();
         return inflatedView;
     }
