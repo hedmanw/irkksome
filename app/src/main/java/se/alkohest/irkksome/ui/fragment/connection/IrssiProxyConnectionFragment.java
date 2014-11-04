3cbf3c6d33f3e5f712c2b5534702424507796d50
Wilhelm Hedman <w.hedman@gmail.com>
2014-11-04 15:45:01 +0100

    Irssi-proxy password is saved

--- app/src/main/java/se/alkohest/irkksome/ui/fragment/connection/IrssiProxyConnectionFragment.java
+++ app/src/main/java/se/alkohest/irkksome/ui/fragment/connection/IrssiProxyConnectionFragment.java
@@ -43,7 +43,7 @@ public class IrssiProxyConnectionFragment extends AbstractConnectionFragment {
             setFieldValue(inflatedView, R.id.server_connect_host, templateConnection.getHost());
             setFieldValue(inflatedView, R.id.server_connect_port, String.valueOf(templateConnection.getPort()));
             setFieldValue(inflatedView, R.id.server_connect_username, templateConnection.getUsername());
-//            setFieldValue(inflatedView, R.id.server_connect_password, templateConnection.getPassword());
+            setFieldValue(inflatedView, R.id.server_connect_password, templateConnection.getPassword());
             setFieldValue(inflatedView, R.id.server_connect_sshHost, templateConnection.getSshHost());
             setFieldValue(inflatedView, R.id.server_connect_sshUser, templateConnection.getSshUser());
 //            setFieldValue(inflatedView, R.id.server_connect_sshPass, templateConnection.getSshPass());
