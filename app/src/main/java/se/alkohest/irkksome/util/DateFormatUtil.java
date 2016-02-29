f7cb1c4eb584a975218b559d23274aa2f7b4a014
Wilhelm Hedman <hedmanw@student.chalmers.se>
2016-02-29 16:31:55 +0100

    Timestamps are formatted properly

--- app/src/main/java/se/alkohest/irkksome/util/DateFormatUtil.java
+++ app/src/main/java/se/alkohest/irkksome/util/DateFormatUtil.java
@@ -1,31 +1,34 @@
 package se.alkohest.irkksome.util;
 
+import android.util.Log;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
-/**
- * Created by wilhelm 2014-11-01.
- */
 public class DateFormatUtil {
     private static final DateFormat timestampFormat = new SimpleDateFormat("HH:mm");
     private static final DateFormat timestampDayFormat = new SimpleDateFormat("HH:mm d MMM");
+    private static final DateFormat timestampYearFormat = new SimpleDateFormat("HH:mm d MMM y");
 
     public static String getTimestamp(Date date) {
         return timestampFormat.format(date);
     }
 
     public static String getTimeDay(Date date) {
-        Calendar today = Calendar.getInstance();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
+        long timeDiff = TimeUnit.DAYS.convert(new Date().getTime()-date.getTime(), TimeUnit.MILLISECONDS);
 
-        if (today.get(Calendar.YEAR) == calendar.get(Calendar.YEAR) && today.get(Calendar.DAY_OF_YEAR) > calendar.get(Calendar.DAY_OF_YEAR)) {
-            return timestampDayFormat.format(date);
-        } else {
+        if (timeDiff <= 0) {
             return timestampFormat.format(date);
+        }
+        else if (timeDiff < 365) {
+            return timestampDayFormat.format(date);
+        }
+        else {
+            return timestampYearFormat.format(date);
         }
     }
 }
